d882639fe4b8e6eaa03f3dd70caba02802ab20e8
Siddhant Tandon <siddhant.tandon@hotmail.com>
2023-09-09 17:40:56 +1000

    Initial Commit

---
+++ The Matrix Screensaver/The_Matrix_ScreensaverView.h
@@ -0,0 +1,12 @@
+//
+//  The_Matrix_ScreensaverView.h
+//  The Matrix Screensaver
+//
+//  Created by Siddhant Tandon on 9/9/2023.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface The_Matrix_ScreensaverView : ScreenSaverView
+
+@end
---
+++ The Matrix Screensaver/The_Matrix_ScreensaverView.m
@@ -0,0 +1,51 @@
+//
+//  The_Matrix_ScreensaverView.m
+//  The Matrix Screensaver
+//
+//  Created by Siddhant Tandon on 9/9/2023.
+//
+
+#import "The_Matrix_ScreensaverView.h"
+
+@implementation The_Matrix_ScreensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
